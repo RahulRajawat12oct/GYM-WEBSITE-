6c95b11644c0926dcac99d173214be6d884cc16d
RahulRajawat12oct <140854490+RahulRajawat12oct@users.noreply.github.com>
2023-08-26 04:40:36 +0530

    Add files via upload

---
+++ js/javaScript.js
@@ -0,0 +1,22 @@
+
+// function hidemenu() {
+//     var m = document.getElementById("menu");
+//        m.style.display = "none";
+//        var n = document.getElementById("cross");
+//        n.style.display = "block";
+       
+
+
+    
+//     // if (m.style.display == "none")
+//     //     m.style.display = "block";
+//     // else
+//     //    m.style.display = "none";
+// }
+
+// function showmenu() {
+//     var m = document.getElementById("menu");
+//     m.style.display = "block";
+//     var n = document.getElementById("cross");
+//     n.style.display = "none";
+// }
